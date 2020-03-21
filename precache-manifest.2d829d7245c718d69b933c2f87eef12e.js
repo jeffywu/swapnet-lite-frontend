7ef2209f1ce98c29d37800be85a2e459edc9f736
Jeff Wu <jeffywu@pm.me>
2020-03-20 19:34:37 -0700

    Updates

---
+++ precache-manifest.2d829d7245c718d69b933c2f87eef12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2548d2c796974e7262ebc4c1be42bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d75de5bba60b2e03ec",
+    "url": "/static/css/main.4f46c5a5.chunk.css"
+  },
+  {
+    "revision": "8c8857d8007da3a465fa",
+    "url": "/static/js/2.7464130e.chunk.js"
+  },
+  {
+    "revision": "76170770bb93fd422c682169067e7a4b",
+    "url": "/static/js/2.7464130e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d75de5bba60b2e03ec",
+    "url": "/static/js/main.da61499b.chunk.js"
+  },
+  {
+    "revision": "6b74b0579a30b981c9aa",
+    "url": "/static/js/runtime-main.c8d75b3c.js"
+  }
+]);
